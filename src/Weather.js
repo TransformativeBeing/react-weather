ddc17076faa9d9cacd49ff7d95c319e0d4282597
TransformativeBeing <83785114+TransformativeBeing@users.noreply.github.com>
2021-08-20 12:03:00 -0700

    Created Weather component

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Weather() {
+    return ( <
+        div className = "Weather" >
+        <
+        h1 > I 'm here!!!</h1> < /
+        div >
+    );
+}
